87dd1587e4bf803b8fd7d50cb81b4ca6e2b19a18
Joe Attardi <jattardi@salesforce.com>
2020-02-20 10:27:50 -0500

    Fail chdir if the target is not a directory

--- src/builtins/chdir.js
+++ src/builtins/chdir.js
@@ -14,13 +14,17 @@ module.exports = function chdir(command, dir) {
 
   const absolutePath = command[0] === '/' ? path.resolve(actualDir) : path.resolve(state.getWorkingDirectory(), actualDir);
 
-  if (fs.existsSync(absolutePath)) {
-    lastDirectory = state.getWorkingDirectory();
-    state.setWorkingDirectory(absolutePath);
-    return 0;
+  if (!fs.existsSync(absolutePath)) {
+    process.stderr.write(`${command}: no such file or directory: ${dir}`);
+    process.stderr.write(os.EOL);
+    return 1;
+  } else if (!fs.statSync(absolutePath).isDirectory()) {
+    process.stderr.write(`${command}: not a directory: ${absolutePath}`);
+    process.stderr.write(os.EOL);
+    return 1;
   }
 
-  process.stderr.write(`${command}: no such file or directory: ${dir}`);
-  process.stderr.write(os.EOL);
-  return 1;
+  lastDirectory = state.getWorkingDirectory();
+  state.setWorkingDirectory(absolutePath);
+  return 0;
 };
