5c388c884009f325b5bad40eb90ad83c6efab6b6
Joe Attardi <jattardi@salesforce.com>
2020-02-20 09:50:24 -0500

    Add export builtin

---
+++ src/builtins/export.js
@@ -0,0 +1,9 @@
+const state = require('../state');
+
+module.exports = function exportEnv(command, arg) {
+  const index = arg.indexOf('=');
+  const key = arg.slice(0, index);
+  const value = arg.slice(index + 1);
+  
+  state.setEnv(key, value);
+};
--- src/builtins/index.js
+++ src/builtins/index.js
@@ -1,3 +1,4 @@
 exports.cd = exports.chdir = require('./chdir');
 exports.pwd = require('./pwd');
 exports.echo = require('./echo');
+exports['export'] = require('./export');
